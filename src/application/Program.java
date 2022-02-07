1033cade777fd82a10d015022326fbe18a7f4a8e
Pedro Henrique Pinto de Lacerda <pedrohenrique73490@gmail.com>
2022-02-07 19:02:13 -0300

    Working with inheritance studies

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
-import application.entities.Account;
-import application.entities.SavingsAccount;
+import application.entities.Employee;
+import application.entities.OutsourcedEmployee;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -14,16 +16,44 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Account x = new SavingsAccount(1000, "Maria", 1000.0, 0.01);
-        Account y = new Account(1001, "Alex", 1000.0);
+        List<Employee> employees = new ArrayList<>();
 
-        x.withdraw(50.0);
-        y.withdraw(50.0);
+        System.out.print("Enter the number of employees:");
+        int number = sc.nextInt();
+
+        for (int i = 1; i <= number; i++) {
+
+            System.out.println("Employee #" + i + " data:");
+
+            System.out.print("Outsourced? (y/n)");
+            char answer = sc.next().charAt(0);
+
+            System.out.println("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+
+            System.out.println("Hours: ");
+            Integer hours = sc.nextInt();
+
+            System.out.println("Value per hour: ");
+            Double valuePerHour = sc.nextDouble();
+
+            if (answer == 'y') {
+                System.out.print("Additional charge: ");
+                Double additionalCharge = sc.nextDouble();
+                employees.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+            } else {
+                employees.add(new Employee(name, hours, valuePerHour));
+            }
+        }
 
 
-        System.out.println(x.getBalance());
-        System.out.println(y.getBalance());
+        System.out.println();
+        System.out.println("Payments: ");
+
+        for (Employee emp : employees) {
+            System.out.println(emp.getName() + ": $ " + String.format("%.2f", emp.payment()));
+        }
 
     }
-
 }
