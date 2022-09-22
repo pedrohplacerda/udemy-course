a3c4856e4c7090a41f33b172ac4ff3eef69a6b69
Pedro Henrique Pinto de Lacerda <pedrohenrique73490@gmail.com>
2022-09-22 18:29:55 -0300

    Working with files studies #2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,27 +1,39 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Objects;
-import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        File file = new File("in.txt");
-        Scanner sc = null;
+        String path = "in.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
         try {
-            sc = new Scanner(file);
-            while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
             }
-        }  catch (IOException ex) {
-            System.out.println("Error: " + ex.getMessage() );
-        }
-        finally {
-            if (Objects.nonNull(sc)) {
-                sc.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
