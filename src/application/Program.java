214f17e2c1c04b2d15c7fbe2c5307c2db7196f4a
Pedro Henrique Pinto de Lacerda <pedrohenrique73490@gmail.com>
2022-09-22 18:39:39 -0300

    Working with files studies #4

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,21 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 
     public static void main(String[] args) {
-        String path = "in.txt";
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String line = br.readLine();
-            while (line != null) {
-                System.out.println(line);
-                line = br.readLine();
+        String path = "c:\\temp\\out.txt";
+        String[] lines = new String[]{"Good morning", "Good afternoon", "Good night"};
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
             }
         } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
